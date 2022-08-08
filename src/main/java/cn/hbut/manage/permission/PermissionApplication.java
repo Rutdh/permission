8ad65a6f769fd15ffdba5e66c3eede021ed1cc5f
Rutdh <1320957532@qq.com>
2022-08-08 17:15:06 +0800

    测试,加一个空格

--- src/main/java/cn/hbut/manage/permission/PermissionApplication.java
+++ src/main/java/cn/hbut/manage/permission/PermissionApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PermissionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PermissionApplication.class, args);
+         SpringApplication.run(  PermissionApplication.class, args);
     }
 
 }
